b6b79f71e5aedddceeacfdfc4e93a52ee6f09914
2Erik <erik19yogo@gmail.com>
2025-11-10 21:08:32 -0500

    Cambio de imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,8 @@
 function saludar(){
+
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./image/b.gif";
+
     console.log("Ingresa a saludar");
 }
