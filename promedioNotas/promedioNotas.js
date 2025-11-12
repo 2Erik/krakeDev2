01b2af522406b5b572a07325edec225d063dc38d
2Erik <erik19yogo@gmail.com>
2025-11-11 22:13:27 -0500

    Agregado imagenes de aprobado y desaprobado

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -6,4 +6,7 @@ function calcularPromedioNotas(){
     const promedio=calcularPromedio(nota1, nota2, nota3);
 
     mostrarTexto("lblNota", promedio.toFixed(2));
+
+    if(promedio>7) mostrarImagen("imgAprobado", "a.gif");
+    else mostrarImagen("imgAprobado", "b.gif");
 }
