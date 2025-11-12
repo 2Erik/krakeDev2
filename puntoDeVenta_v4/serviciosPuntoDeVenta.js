7f8766e09daf44b874688e6d127d9f9e6ac5a1a2
2Erik <erik19yogo@gmail.com>
2025-11-11 20:27:24 -0500

    Mostrar el total en pantall

--- puntoDeVenta_v4/serviciosPuntoDeVenta.js
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -4,4 +4,4 @@ function calcularIVA(monto){return monto * 0.12}
 
 function calcularSubTotal(precio, cantidad){return precio*cantidad}
 
-function calcularTotal(subTotal,descuento,iva){subTotal-descuento+iva}
+function calcularTotal(subTotal,descuento,iva){return subTotal-descuento+iva}
