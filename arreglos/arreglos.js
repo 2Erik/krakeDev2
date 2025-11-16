5df21012a710e7fbd3711a700ab6cb1a54e6b1a5
2Erik <erik19yogo@gmail.com>
2025-11-15 23:29:41 -0500

    Calcular promedio notas

---
+++ arreglos/arreglos.js
@@ -0,0 +1,66 @@
+let notas=[];
+
+agregarElementos=function(){
+    notas.push(5);
+    notas.push(10);
+    console.log(notas.length);
+}
+
+recorrerArreglo=function(){
+    let notaR;
+    for(let i=0;i<notas.length;i++){
+        notaR=notas[i];
+        console.log(notaR);
+    }
+}
+
+probraAgregar=function(){
+    let notaRecuperada;
+    notaRecuperada=recuperarInt("txtNota");
+    agregarNota(notaRecuperada);
+}
+
+agregarNota=function(nota){
+    notas.push(nota);
+    mostrarNotas();
+}
+
+calcularPromedio=function(){
+    let sumaNotas=0;
+    let promedio;
+
+    for(let i=0;i<notas.length;i++){
+        sumaNotas += notas[i];
+    }
+
+    promedio=(sumaNotas)/notas.length;
+    return promedio;
+}
+
+ejecutarPromedio=function(){
+    let resultadoPromedio;
+    resultadoPromedio=calcularPromedio();
+    mostrarTexto("lblPromedio",resultadoPromedio);
+}
+
+generarTabla=function(){
+    let contenidoTabla="";
+    let cmpTabla=document.getElementById("divTabla");
+    contenidoTabla+="<table><tr><td>UNO</td></tr>"+
+    "<tr><td>DOS</td></tr></table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
+
+mostrarNotas=function(){
+    let cmpTabla=document.getElementById("divTabla");
+    let contenidoTabla="<table><tr><th>NOTAS</th></tr>";
+    let miNota;
+    for(let i=0;i<notas.length;i++){
+        miNota=notas[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</td></tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
