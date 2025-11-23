1aa087079db904b3abbd52a548a524d93d1a887f
2Erik <erik19yogo@gmail.com>
2025-11-23 10:19:28 -0500

    Ultimo trabajo en grupo

---
+++ trabajo grupla/movimientos.js
@@ -0,0 +1,65 @@
+movimientos=[
+    {numeroCuenta:"02234567",monto:10.24,tipo:"D"},
+    {numeroCuenta:"02345211",monto:45.90,tipo:"D"},
+    {numeroCuenta:"02234567",monto:65.23,tipo:"C"},
+    {numeroCuenta:"02345211",monto:65.23,tipo:"C"},
+    {numeroCuenta:"02345211",monto:12.0,tipo:"D"},
+]
+
+cargar=function(){
+    mostrarComponente("divMovimientos");
+    ocultarComponente("divCuentas");
+    ocultarComponente("divTransacciones");
+    
+}
+
+filtrarMovimientos=function(numeroCuenta){
+    let movimientosCuenta=[];
+    //Se barre el arreglo de movimientos
+    for(let i=0;i<movimientos.length;i++){
+        if(movimientos[i].numeroCuenta==numeroCuenta){
+            movimientosCuenta.push(movimientos[i]);
+        }
+    }
+    //En cada iteración, verifica si el numero de cuenta del movimiento es igual al que recibe como parametro
+    for(let i=0;i<movimientosCuenta.length;i++){
+        if(movimientosCuenta[i].tipo=="D"){
+            movimientosCuenta[i].monto *= -1;
+        }
+    }
+
+    //En caso de serlo, agrega la cuenta al arreglo movimientosCuenta
+    mostrarMovimientos(movimientosCuenta);
+    //Invoca a mostrarMovimientos, pasándole como parámetro movimientosCuenta
+}
+
+/*
+    Recibe un arreglo con los movimientos que va a mostrar en pantalla
+*/
+mostrarMovimientos=function(misMovimientos){
+    //Muestra en pantalla una tabla con los movimientos que recibe en misMovimientos
+    let cmpTabla= document.getElementById("tablaMovimientos");
+
+    //Columnas: NUMERO CUENTA, MONTO, TIPO
+    let contenidoTabla = "<table><tr>" + "<th> CUENTA </th>" + "<th> MONTO </th>" + "<th> OPERACION </th>" + "</tr>";
+    //Si ya pinta correctamente la tabla, hacer el siguiente cambio:
+    let elementoCuentas;
+    for(let i=0; i<misMovimientos.length; i++){
+        elementoCuentas = misMovimientos[i];
+        contenidoTabla += "<tr> <td>" + elementoCuentas.numeroCuenta + "</td>" +
+        "<td>" + elementoCuentas.monto + "</td>" +
+        "<td>" + elementoCuentas.tipo + "</td> </tr>";
+    }
+    //Si el tipo es D(DEBITO), mostrar el monto en negativo (multiplicar por -1)
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
+    //Si el tipo es C(CREDITO), mostrar el monto en positivo (tal como está guardado)
+}
+ejecutarMovimientos=function(){
+    let numeroCuenta= recuperarTexto("txtNumCuenta3");
+    filtrarMovimientos(numeroCuenta);
+}
+
+
+
+
